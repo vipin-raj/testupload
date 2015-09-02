8b5e0028990fc3eb7e2d0b50c47b179cfa01c267
Vipin Raj <vipin.raj@experionglobal.com>
2015-09-02 16:12:06 +0530

    second change

--- retrofitdemo/app/src/main/java/experion/com/retrofitdemo/MainActivity.java
+++ retrofitdemo/app/src/main/java/experion/com/retrofitdemo/MainActivity.java
@@ -16,7 +16,7 @@ import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
-
+//hai
 public class MainActivity extends ActionBarActivity {
 
     Button click;
